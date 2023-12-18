aba7166602c3e53444f161d76b099221afdde3ec
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-12-18 15:39:44 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import React from "react";
-import Second from "./Second/Second";
-import Crousel from "./Third/Crousel";
-import Nav from "./First/Nav";
+
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
-import {
-  BrowserRouter,
-  Route,
-  Router,
-  createBrowserRouter,
-} from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Router>
-          <Route>
-            <Nav />
-            <Second />
-            <Crousel />
-          </Route>
-        </Router>
-
-      </BrowserRouter> */}
-      <Nav />
-      <Second />
-      <Crousel />
+      
 
       <ToastContainer />
     </>
